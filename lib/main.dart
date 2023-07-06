b6350d9c7629c060fedce709d579cbc248b3c5fa
Sparsh Kapoor <kapsparsh4@gmail.com>
2023-07-06 23:40:27 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Material(
+        child: Center(
+          child: Container(
+          child: const Text("Welcome to my app"),
+            ),
+        ),
+      ) ,
+    );
+  }
+}
