36b9b3fc90626c368f9210e2bf5ad7d64279e092
Sparsh Kapoor <kapsparsh4@gmail.com>
2023-07-08 23:28:47 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalogue_1/pages/home_page.dart';
 import 'package:flutter_catalogue_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(MyApp());
@@ -15,11 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
     
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark
       ),
-      initialRoute: "/Home",
+      initialRoute: "/Login",
       routes: {
         "/":(context) => LoginPage(),
         "/Home":(context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,15 +5,54 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Page",style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold
-          
-        ),),
-    ),
+    return  Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.network(
+            'https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg',
+            fit: BoxFit.cover,),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text("Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold
+            )),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical:16.0,horizontal: 32.0),
+              child: Column(children: [
+                TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: (){
+                print("hi codepur");
+
+              }, child: Text("Login"),
+              style: TextButton.styleFrom(),)
+              ],),
+            )
+        ],
+        
+      )
     );
   }
 }
