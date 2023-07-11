4f22919d1b0b27265c94ff9ca74d6a0752d73505
Sparsh Kapoor <kapsparsh4@gmail.com>
2023-07-11 18:24:42 +0530

    day10 completed

---
+++ lib/models/catalogue.dart
@@ -0,0 +1,17 @@
+class Item{
+  final String id;
+  final String description;
+  final String name;
+  final String color;
+  final String Imageurl;
+  final num price;
+
+  Item(this.id, this.description, this.name, this.color, this.Imageurl, this.price);
+  final products =[Item(
+    id, 
+    description, 
+    name, 
+    color, 
+    Imageurl,
+     price)];
+}
