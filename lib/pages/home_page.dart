6428cab8bba565cb74c1dd484c22f721a61cbca4
Sparsh Kapoor <kapsparsh4@gmail.com>
2023-07-12 19:39:06 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalogue_1/models/catalogue.dart';
 import 'package:flutter_catalogue_1/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-   int days=30;
-   String name="Codepur";
+class HomePage extends StatefulWidget {
 
   HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+   int days=30;
+   String name="Codepur";
+    @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData()async{
+      var catalogueJson = await rootBundle.loadString("lib/assets/files/catalogue.json");
+    final decodeData = jsonDecode(catalogueJson);
+    var productsData =decodeData["products"];
+    print(productsData);
+  }
+   
 
   @override
   Widget build(BuildContext context) {
